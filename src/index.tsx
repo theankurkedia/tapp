0cf325b09d9ddeb4147586c050b621f980aee79f
Ankur Kedia <theankurkedia@gmail.com>
2026-03-06 23:43:33 +0100

    chore: added vercel analytics

--- src/index.tsx
+++ src/index.tsx
@@ -3,10 +3,12 @@ import { createRoot } from 'react-dom/client';
 import './index.css';
 import App from './App';
 import { registerSW } from 'virtual:pwa-register';
+import { Analytics } from '@vercel/analytics/react';
 
 createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
     <App />
+    <Analytics />
   </React.StrictMode>
 );
 
